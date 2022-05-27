adc22cf3aec06ac7a852748e4f9ca2914f9cb528
Ramiro Garces <ramgarces24@gmail.com>
2022-05-27 05:18:05 -0500

    Add function to create squares for the container

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 const container = document.querySelector("#grid-container");
+
+function makeSquares(cols, rows) {
+    // Setting container rows and cols to what is passed to the function
+    container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    // Creating divs (squares) equal to (cols * rows)
+    for (i = 0; i < (cols * rows); i++) {
+        const square = document.createElement('div');
+        square.style.border = '1px solid red';
+        container.appendChild(square);
+    }
+}
+// Calling the function to make a 16 x 16 grid
+makeSquares(16, 16);
